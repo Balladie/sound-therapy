2b451df1beae3202ddd5d51dddb6256a1b331703
Balladie <ssonpull519@gmail.com>
2020-02-28 15:20:37 +0900

    2020.02.28

---
+++ app/src/main/java/com/balladie/soundtherapy/network/model/SoundInfo.kt
@@ -0,0 +1,5 @@
+package com.balladie.soundtherapy.network.model
+
+data class SoundInfo(
+    val title: String
+)
---
+++ app/src/main/java/com/balladie/soundtherapy/network/service/SoundLinkService.kt
@@ -0,0 +1,16 @@
+package com.balladie.soundtherapy.network.service
+
+import com.balladie.soundtherapy.network.model.SoundInfo
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface SoundLinkService {
+
+    @GET("sound")
+    fun getSoundLink(
+        @Query("mode") mode: Int,
+        @Query("latitude") latitude: Float,
+        @Query("longitude") longitude: Float
+    ): Single<SoundInfo>
+}
